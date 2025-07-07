d8a84e77e78a8f20750ccd49d6b9778cb6fcbeaa
Luiz <nandoluizprimeiro@gmail.com>
2025-07-07 15:07:38 -0300

    Pequena estilização

---
+++ app/index.jsx
@@ -0,0 +1,27 @@
+import { Background } from "@react-navigation/elements";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text
+        style={styles.text}
+      >
+        Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles=StyleSheet.create({
+  container:{
+    flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#021123',
+  },
+  text:{
+    color: '#fff',
+  }
+})
